aa2fb357d09e791824b8b30699bac5ca8ae82032
rchmn17 <ptr330rachman@gmail.com>
2025-05-15 04:43:27 +0700

    fitur Buat Akun

--- PemesananTiketKereta/src/User.java
+++ PemesananTiketKereta/src/User.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 class User {
@@ -27,6 +30,60 @@ class User {
     }
 
     public String getHistory() {
+        return "";
+    }
 
+    public static void buatAkun(String UserName, String password, String Nama) {
+        String path = "./Users/"+UserName;
+        String path2 = path +"/";
+        File pathDir = new File(path);
+        File[] pathFile = new File[3];
+        pathFile[0] = new File(path2+"Password.txt");
+        pathFile[1] = new File(path2+"Id.txt");
+        pathFile[2] = new File(path2+"History.txt");
+        
+
+        // bikin folder
+        if (!pathDir.exists()) {
+            boolean tes = false;
+            try {
+                pathDir.mkdir();
+                tes = true;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            if (tes) {
+                System.out.println("folderBerhasilDibuat");
+            }
+        } else {
+            System.out.println("folderSudahAda");
+        }
+
+
+        // bikin setUp file
+        for (File p : pathFile) {
+            if (!p.exists()) {
+                boolean tes = false;
+                try {
+                    p.createNewFile();
+                    tes = true;
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                if (tes) {
+                    System.out.println("UserBerhasilDisetup");
+                }
+            } else {
+                System.out.println("Eror");
+            }
+        }
+
+        //menulis isi file
+        
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(p))) {
+                writer.write(password);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
     }
 }
