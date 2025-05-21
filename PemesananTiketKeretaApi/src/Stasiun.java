a4a949ac042347e6b664a1012549f79e03f92d07
yoga-kurniawan <yogakurniawan02@student.ub.ac.id>
2025-05-21 20:39:29 +0700

    Reapply "Merge pull request #22 from rchmn17/umar"
    This reverts commit ac9aadd44a70cd9574577fb1c55fac5ddb5c891b.

---
+++ PemesananTiketKeretaApi/src/Stasiun.java
@@ -0,0 +1,27 @@
+class Stasiun {
+    String nama;
+    String kota;
+    
+    Stasiun(String nama, String kota) {
+        this.nama = nama;
+        this.kota = kota;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setKota(String kota) {
+        this.kota = kota;
+    }
+
+    public String getKota() {
+        return kota;
+    }
+
+
+}
---
+++ PemesananTiketKeretaApi/src/TipeKereta.java
@@ -0,0 +1,15 @@
+public enum TipeKereta {
+    EKONOMI(100_000),
+    BISNIS(250_000),
+    EXECUTIVE(500_000);
+
+    private final int harga;
+
+    private TipeKereta(int harga) {
+        this.harga = harga;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+}
