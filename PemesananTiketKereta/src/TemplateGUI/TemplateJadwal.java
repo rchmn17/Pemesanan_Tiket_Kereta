2fede121e1720d1e28484fa534661dcc032a59a5
yoga-kurniawan <yogakurniawan02@student.ub.ac.id>
2025-06-02 20:23:04 +0700

    fix list

--- PemesananTiketKereta/src/TemplateGUI/TemplateJadwal.java
+++ PemesananTiketKereta/src/TemplateGUI/TemplateJadwal.java
@@ -31,15 +31,17 @@ public class TemplateJadwal extends javax.swing.JPanel {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        namaKereta = new javax.swing.JLabel();
-        tipeKereta = new javax.swing.JLabel();
-        StasiunAwal = new javax.swing.JLabel();
-        jamPergi = new javax.swing.JLabel();
-        StasiunAkhir = new javax.swing.JLabel();
-        jamSampai = new javax.swing.JLabel();
-        harga = new javax.swing.JLabel();
-        btnPesan = new javax.swing.JButton();
-        kursiTersedia = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel13 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -52,155 +54,176 @@ public class TemplateJadwal extends javax.swing.JPanel {
             .addGap(0, 100, Short.MAX_VALUE)
         );
 
-        setBackground(new java.awt.Color(153, 204, 255));
+        setBackground(new java.awt.Color(255, 177, 111));
         setForeground(new java.awt.Color(153, 204, 255));
-        setMinimumSize(new java.awt.Dimension(362, 86));
-        setPreferredSize(new java.awt.Dimension(362, 86));
+        setMaximumSize(new java.awt.Dimension(32000, 86));
+        setMinimumSize(new java.awt.Dimension(362, 100));
+        setPreferredSize(new java.awt.Dimension(362, 100));
 
-        namaKereta.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 12)); // NOI18N
-        namaKereta.setText("Kereta (01)");
+        jLabel6.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 14)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setText("Kereta (01)");
 
-        tipeKereta.setText("Ekonomi (EK)");
+        jLabel7.setText("Ekonomi (EK)");
 
-        StasiunAwal.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 10)); // NOI18N
-        StasiunAwal.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        StasiunAwal.setText("MALANG");
+        jPanel2.setBackground(new java.awt.Color(255, 249, 238));
 
-        jamPergi.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 12)); // NOI18N
-        jamPergi.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jamPergi.setText("08.00");
+        jLabel9.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        jLabel9.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel9.setText("08.00");
 
-        StasiunAkhir.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 10)); // NOI18N
-        StasiunAkhir.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        StasiunAkhir.setText("SOLO");
+        jLabel8.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(255, 174, 114));
+        jLabel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel8.setText("MALANG");
 
-        jamSampai.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 12)); // NOI18N
-        jamSampai.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jamSampai.setText("13.00");
+        jLabel11.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        jLabel11.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel11.setText("13.00");
 
-        harga.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 12)); // NOI18N
-        harga.setText("Rp. 200.000,-");
+        jLabel10.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel10.setForeground(new java.awt.Color(255, 174, 114));
+        jLabel10.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel10.setText("SOLO");
 
-        btnPesan.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 12)); // NOI18N
-        btnPesan.setText("PESAN");
-        btnPesan.addActionListener(new java.awt.event.ActionListener() {
+        jLabel12.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        jLabel12.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLabel12.setText("Rp. 200.000,-");
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel1.setText("to");
+
+        jLabel13.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        jLabel13.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel13.setText("40");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel10, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel11, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 7, Short.MAX_VALUE)
+                .addComponent(jLabel13, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(27, 27, 27)
+                .addComponent(jLabel12, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel10)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel11)
+                    .addComponent(jLabel9)
+                    .addComponent(jLabel13)
+                    .addComponent(jLabel12))
+                .addContainerGap(18, Short.MAX_VALUE))
+        );
+
+        jPanel2Layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {jLabel10, jLabel11, jLabel8, jLabel9});
+
+        jButton2.setBackground(new java.awt.Color(51, 89, 167));
+        jButton2.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 12)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(255, 255, 255));
+        jButton2.setText("PESAN");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnPesanActionPerformed(evt);
+                jButton2ActionPerformed(evt);
             }
         });
-
-        kursiTersedia.setText("Kursi tersedia");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(10, 10, 10)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(namaKereta)
-                    .addComponent(tipeKereta))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jamPergi, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(StasiunAwal, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(StasiunAkhir, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jamSampai, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(kursiTersedia)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(btnPesan, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(27, 27, 27)
+                        .addComponent(jLabel7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(174, 174, 174)
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(14, 14, 14))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(harga, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addContainerGap())))
         );
-
-        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {StasiunAkhir, StasiunAwal, jamPergi, jamSampai});
-
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(12, 12, 12)
-                        .addComponent(harga)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(btnPesan, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(kursiTersedia)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(22, 22, 22)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(StasiunAkhir)
-                            .addComponent(StasiunAwal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(namaKereta))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jamPergi)
-                            .addComponent(jamSampai)
-                            .addComponent(tipeKereta))))
-                .addContainerGap(11, Short.MAX_VALUE))
+                .addGap(10, 10, 10)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(jLabel7)
+                    .addComponent(jButton2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(9, Short.MAX_VALUE))
         );
-
-        layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {StasiunAkhir, StasiunAwal, jamPergi, jamSampai});
-
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnPesanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPesanActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_btnPesanActionPerformed
+    }//GEN-LAST:event_jButton2ActionPerformed
 
-    public JButton getBtnPesan() {
-        return btnPesan;
+    public JButton getjButton2() {
+        return jButton2;
     }
 
-    public JLabel getStasiunAkhir() {
-        return StasiunAkhir;
+    public JLabel getjLabel10() {
+        return jLabel10;
     }
 
-    public JLabel getJamSampai() {
-        return jamSampai;
+    public JLabel getjLabel11() {
+        return jLabel11;
     }
 
-    public JLabel getHarga() {
-        return harga;
+    public JLabel getjLabel12() {
+        return jLabel12;
     }
 
-    public JLabel getNamaKereta() {
-        return namaKereta;
+    public JLabel getjLabel6() {
+        return jLabel6;
     }
 
-    public JLabel getTipeKereta() {
-        return tipeKereta;
+    public JLabel getjLabel7() {
+        return jLabel7;
     }
 
-    public JLabel getStasiunAwal() {
-        return StasiunAwal;
+    public JLabel getjLabel8() {
+        return jLabel8;
     }
 
-    public JLabel getJamPergi() {
-        return jamPergi;
-    }
-
-    public JLabel getKursiTersedia() {
-        return kursiTersedia;
+    public JLabel getjLabel9() {
+        return jLabel9;
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel StasiunAkhir;
-    private javax.swing.JLabel StasiunAwal;
-    private javax.swing.JButton btnPesan;
-    private javax.swing.JLabel harga;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JLabel jamPergi;
-    private javax.swing.JLabel jamSampai;
-    private javax.swing.JLabel kursiTersedia;
-    private javax.swing.JLabel namaKereta;
-    private javax.swing.JLabel tipeKereta;
+    private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
